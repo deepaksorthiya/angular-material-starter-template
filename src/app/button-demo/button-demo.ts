30c7aa50715080ddc0cf7a99fc98e873c6b8c128
Deepak Katariya <deepak.sorthiya@gmail.com>
2026-03-30 10:03:57 +0530

    button demo with var overrode

---
+++ src/app/button-demo/button-demo.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatAnchor,
+  MatButton,
+  MatButtonAppearance,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton,
+} from '@angular/material/button';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatIcon } from '@angular/material/icon';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatTooltip } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'button-demo',
+  templateUrl: './button-demo.html',
+  styleUrl: './button-demo.scss',
+  imports: [
+    FormsModule,
+    MatAnchor,
+    MatButton,
+    MatCheckbox,
+    MatFabAnchor,
+    MatFabButton,
+    MatIcon,
+    MatIconAnchor,
+    MatIconButton,
+    MatMiniFabAnchor,
+    MatMiniFabButton,
+    MatProgressSpinner,
+    MatTooltip,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonDemo {
+  isDisabled = false;
+  showProgress = false;
+  clickCounter = 0;
+  toggleDisable = false;
+  tooltipText = 'This is a button tooltip!';
+  disabledInteractive = false;
+  appearances: MatButtonAppearance[] = ['text', 'elevated', 'outlined', 'filled', 'tonal'];
+}
